9effc9ed01db2ed35b9d9c4cd0ba70d577c95ebb
Piotr Mankowski <pmanko@uw.edu>
2024-01-10 11:39:14 -0800

    Pilot Update (#5)
    * Added traefik reverse proxy
    
    * Updates
    
    * Upgrades

--- mk.sh
+++ mk.sh
@@ -3,17 +3,14 @@
 ./build-image.sh
 
 #./instant package remove -n shared-health-record --env-file .env
-#./instant package remove -n client-registry-opencr --env-file .env
-#./instant package remove -n openhim-mediator-fhir-converter --env-file .env
-#./instant package remove -n reverse-proxy-traefik --env-file .env
-./instant package remove -n interoperability-layer-openhim --env-file .env
+./instant package remove -n client-registry-opencr --env-file .env
+# ./instant package remove -n fhir-datastore-hapi-fhir --env-file .env
+# ./instant package remove -n message-bus-kafka --env-file .env
+# ./instant package remove -n openhim-mediator-fhir-converter --env-file .env
 
-# ./instant package init -n client-registry-opencr --env-file .env
-#./instant package init -n openhim-mediator-fhir-converter --env-file .env
-#./instant package init -n reverse-proxy-traefik --env-file .env
-./instant package init -n interoperability-layer-openhim --env-file .env
-#./instant package init -n shared-health-record --env-file .env
-#./instant package init -n openhim-mediator-omang-service --env-file .env
+# ./instant package init -n openhim-mediator-fhir-converter --env-file .env -d
+# ./instant package init -n message-bus-kafka --env-file .env
+# ./instant package init -n fhir-datastore-hapi-fhir --env-file .env
+./instant package init -n client-registry-opencr --env-file .env
+#./instant package init -n shared-health-record --env-file .env -d
 
-
-# docker stack deploy         -c ./interoperability-layer-openhim/docker-compose.yml                  --with-registry-auth         openhim
