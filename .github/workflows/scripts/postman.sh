cc147e230a6547a3bb90e2b87101442223c2a1d9
MAKOBA REAGAN PATRICK <reaganmeant@gmail.com>
2024-01-30 12:29:08 +0000

    Add

--- .github/workflows/scripts/postman.sh
+++ .github/workflows/scripts/postman.sh
@@ -6,9 +6,12 @@ env_file="postman/bwhie.org.postman_environment.json"
 collection_uuids=("1525496-77f89792-c73b-4c75-a450-5cb6f79230d4" "1525496-16a5a3c2-719f-40a6-ad06-a9530d3f2006" "1525496-8be9c19c-9e7b-4316-adea-3604884af5d7" "1525496-e7128a3a-616d-4fc8-8e4a-615990a97756")
 collection_names=("Service Status Tests" "Client Registry Workflow" "Laboratory Order Workflow" "Omang, BDRS & Immigration")
 publish=false
+echo "I have runnnbnb"
 
 # Login to Postman
 postman login --with-api-key "$POSTMAN_API_KEY"
+login_exit_status=$?
+echo "Exit status of postman login command: $login_exit_status"
 echo "I have runn"
 
 # Parse command line options
